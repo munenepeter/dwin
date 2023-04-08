e8db18814058b9f8d01c4dad58c54e0987c954e7
munenepeter <peternjega7@gmail.com>
2023-04-08 19:32:29 +0300

    update name

--- resources/views/livewire/clients/create.blade.php
+++ resources/views/livewire/clients/create.blade.php
@@ -59,10 +59,10 @@
                 <div>
 
                     <x-jet-label for="insurance" value="{{ __('Class of Insurance') }}" />
-                    <select id="insurance" wire:model="insurance" required autofocus class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <select id="insurance" wire:model="insurance_types" required autofocus class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option>Choose class of insurance</option>
                         @if (count($insurances) > 0)
-                        @foreach ($insurances as $insurance)
+                        @foreach ($insurance_types as $insurance)
                         <option value="{{ $underwriter->id }}"> {{ ucfirst($insurance->name) }}</option>
                         @endforeach
                         @else
