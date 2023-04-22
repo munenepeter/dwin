e25b3aaaf2bb2d445f59c0ac0ae5c163f6581df8
Peter Munene <peternjega7@gmail.com>
2023-04-22 22:54:18 +0300

    added icons

--- app/PowerGridThemes/CustomTheme.php
+++ app/PowerGridThemes/CustomTheme.php
@@ -31,11 +31,11 @@ class CustomTheme extends ThemeBase {
             ->div('my-3 bg-white shadow-lg rounded-lg relative')
             ->thead('shadow-sm bg-pg-primary-100 dark:bg-pg-primary-800 border border-pg-primary-200 dark:border-pg-primary-500')
             ->tr('')
-            ->trFilters('bg-blue-100 shadow-sm dark:bg-pg-primary-700')
-            ->th('font-semibold px-2 pr-2 py-3 text-left text-xs font-semibold text-pg-primary-700 tracking-wider whitespace-nowrap dark:text-pg-primary-300')
+            ->trFilters('items-center bg-blue-100 shadow-sm dark:bg-pg-primary-700')
+            ->th('font-semibold px-2 pr-2 py-2 text-left text-xs font-semibold text-pg-primary-700 tracking-wider whitespace-nowrap dark:text-pg-primary-300')
             ->tbody('text-pg-primary-800')
             ->trBody('border border-pg-primary-100 dark:border-pg-primary-400 hover:bg-pg-primary-50 dark:bg-pg-primary-700 dark:odd:bg-pg-primary-800 dark:odd:hover:bg-pg-primary-900 dark:hover:bg-pg-primary-700')
-            ->tdBody('pl-[6px] px-2 py-2 whitespace-nowrap dark:text-pg-primary-200')
+            ->tdBody('pl-[4px] px-2 py-2 whitespace-nowrap dark:text-pg-primary-200')
             ->tdBodyEmpty('px-3 py-2 whitespace-nowrap dark:text-pg-primary-200')
             ->tdBodyTotalColumns('px-2 py-2 whitespace-nowrap dark:text-pg-primary-200 text-sm text-pg-primary-600 text-right space-y-2');
     }
@@ -48,7 +48,7 @@ class CustomTheme extends ThemeBase {
 
     public function actions(): Actions {
         return Theme::actions()
-            ->headerBtn('block w-full bg-pg-primary-50 text-pg-primary-700 border border-pg-primary-200 rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-pg-primary-600 dark:border-pg-primary-500 dark:bg-pg-primary-600 2xl:dark:placeholder-pg-primary-300 dark:text-pg-primary-200 dark:text-pg-primary-300')
+            ->headerBtn('block w-full bg-pg-primary-50 text-pg-primary-700 border border-pg-primary-200 rounded py-2 px-2 leading-tight focus:outline-none focus:bg-white focus:border-pg-primary-600 dark:border-pg-primary-500 dark:bg-pg-primary-600 2xl:dark:placeholder-pg-primary-300 dark:text-pg-primary-200 dark:text-pg-primary-300')
             ->rowsBtn('focus:outline-none text-sm py-2.5 px-4 rounded border');
     }
 
@@ -107,14 +107,14 @@ class CustomTheme extends ThemeBase {
     public function filterSelect(): FilterSelect {
         return Theme::filterSelect()
             ->view($this->root() . '.filters.select')
-            ->base('min-w-[9.5rem]')
+            ->base('min-w-[7.5rem]')
             ->select('appearance-none block bg-white border border-pg-primary-300 text-pg-primary-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-pg-primary-500 w-full active dark:bg-pg-primary-600 dark:text-pg-primary-200 dark:placeholder-pg-primary-100 dark:border-pg-primary-500');
     }
 
     public function filterInputText(): FilterInputText {
         return Theme::filterInputText()
             ->view($this->root() . '.filters.input-text')
-            ->base('min-w-[9.5rem]')
+            ->base('min-w-[7.5rem]')
             ->select('appearance-none block bg-white border border-pg-primary-300 text-pg-primary-700 py-2 px-3 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-pg-primary-500 w-full active dark:bg-pg-primary-600 dark:text-pg-primary-200 placeholder-pg-primary-400 dark:placeholder-pg-primary-100 dark:border-pg-primary-500')
             ->input('w-full block bg-white text-pg-primary-700 border border-pg-primary-300 rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-pg-primary-500 dark:bg-pg-primary-600 dark:text-pg-primary-200 dark:placeholder-pg-primary-100 dark:border-pg-primary-500 placeholder-pg-primary-400');
     }
