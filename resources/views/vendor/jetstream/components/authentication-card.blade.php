80719f5e38b96d9d71379423773f18109598712c
munenepeter <peternjega7@gmail.com>
2023-05-09 18:15:44 +0300

    done for now with front-end

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,5 +1,5 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:p-4 bg-gray-100">
-       <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+<div class="md:min-h-[700px] min-h-[600px] flex flex-col sm:justify-center items-center md:pt-6 p-4 bg-gray-100">
+       <div class="md:max-w-md w-full md:mt-2 mt-10 px-6 py-4 bg-white md:shadow-md shadow-sm overflow-hidden rounded-lg">
         {{ $slot }}
     </div>
 </div>
