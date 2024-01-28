41218d9203f35e40ba31021273453a9d1b9d06ce
munenepeter <peternjega7@gmail.com>
2024-01-28 20:54:07 +0300

    add theme

--- resources/views/layouts/app-layout.blade.php
+++ resources/views/layouts/app-layout.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#799649">
+    <meta name="theme-color" content="#f43f5e">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8">
 
