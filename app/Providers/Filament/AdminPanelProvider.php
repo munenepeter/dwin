f5dbafeebffe712b51dad068b5ed6dace1f2ff6e
munenepeter <peternjega7@gmail.com>
2024-01-28 20:53:19 +0300

    fix: resize the sidenav

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->sidebarWidth('12rem')
             ->colors([
                 'primary' => Color::Rose,
             ])
